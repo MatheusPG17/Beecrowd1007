54d005d01663e314beaa1c07fc54f21ddbe12872
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 15:42:20 -0300

    add comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         int A, B, C, D, DIFERENCA;
         
+        //entrada de dados
         A = leia.nextInt();
         B = leia.nextInt();
         C = leia.nextInt();
         D = leia.nextInt();
         
+        //processamento 
         DIFERENCA = (A * B - C * D);
         
+        //saida de dados
         System.out.println("DIFERENCA = " + DIFERENCA);
         
     }
